2b6f1f620e5c0ba83824874ff6c74410c692bb1d
Nightrose2016 <lrbarrer@gmail.com>
2020-07-16 20:41:11 -0400

    place holder shit

---
+++ src/components/Dashboard.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+const Dashboard = () => {
+    return (
+        <div>
+            <div>
+                <h1>dashboard</h1>
+                <h1>dashboard</h1>
+            </div>
+        </div>
+    )
+}
+export default Dashboard
