90ead0d684468e5630d196c5650e038f4e0836d4
Nightrose2016 <lrbarrer@gmail.com>
2020-07-16 21:31:09 -0400

    Added user experience to dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,13 +1,15 @@
-import React, { Component } from 'react'
+ 
+import React from "react";
 
-const Dashboard = () => {
-    return (
-        <div>
-            <div>
-                <h1>dashboard</h1>
-                <h1>dashboard</h1>
-            </div>
-        </div>
-    )
-}
-export default Dashboard
+const Dashboard = props => {
+  return (
+    <div>
+      <div>
+        <h1>Dashboard</h1>
+        <h1>Status: {props.loggedInStatus}</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
