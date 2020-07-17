63ad8e590fbb329e078f748ddd4e0506973ff215
Nightrose2016 <lrbarrer@gmail.com>
2020-07-16 21:38:04 -0400

    built constructor out

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -5,4 +5,14 @@ export default class Login extends Component {
     constructor(props) {
       super(props);
     }
+
+    this.state = {
+        username: "",
+        password: "",
+        loginErrors: ""
+      };
+  
+      this.handleSubmit = this.handleSubmit.bind(this);
+      this.handleChange = this.handleChange.bind(this);
+    }
 }
