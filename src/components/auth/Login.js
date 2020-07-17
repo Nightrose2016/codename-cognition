479d1d88134601740f052d528f1f55fb0faae06c
Nightrose2016 <lrbarrer@gmail.com>
2020-07-16 21:42:02 -0400

    added handle change and handle submit

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -15,4 +15,32 @@ export default class Login extends Component {
       this.handleSubmit = this.handleSubmit.bind(this);
       this.handleChange = this.handleChange.bind(this);
     }
+    handleChange(event) {
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    }
+    
+    handleSubmit(event) {
+        const { username, password } = this.state;
+    
+        axios.post("http://localhost:3001/sessions",{
+            user: {
+            username: username,
+            password: password
+            }
+        },
+        { withCredentials: true }
+        )
+        .then(response => {
+            if (response.data.logged_in) {
+                this.props.handleSuccessfulAuth(response.data);
+            }
+        })
+        .catch(error => {
+            console.log("login error", error);
+        });
+        event.preventDefault();
+    }
 }
+    
