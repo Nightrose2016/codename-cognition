3d89520fb2ee0e0fb57b26c55f67c1772a9e6d5f
Nightrose2016 <lrbarrer@gmail.com>
2020-07-20 15:28:50 -0400

    More spacing fixs

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -26,35 +26,33 @@ export default class Login extends Component {
     const { username, password } = this.state;
 
     axios.post("http://localhost:3001/sessions",{
-            user: {
-                username: username,
-                password: password
-            }
-            },
-            { withCredentials: true }
-        )
-        .then(response => {
-            if (response.data.logged_in) {
-            this.props.handleSuccessfulAuth(response.data);
-            }
-        })
-        .catch(error => {
-            console.log("login error", error);
-        });
-        event.preventDefault();
-    }
+      user: {
+        username: username,
+        password: password
+      }
+    },
+    { withCredentials: true }
+    )
+    .then(response => {
+      if (response.data.logged_in) {
+        this.props.handleSuccessfulAuth(response.data);
+      }
+    })
+    .catch(error => {
+      console.log("login error", error);
+    });
+    event.preventDefault();
+  }
 
   render() {
     return (
-        <div>
-            <form onSubmit={this.handleSubmit}>
-                <input type="username" name="username" placeholder="username" value={this.state.username} onChange={this.handleChange} required/>
-
-                <input type="password" name="password" placeholder="Password" value={this.state.password} onChange={this.handleChange} required />
-
-                <button type="submit">Login</button>
-            </form>
-        </div>
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <input type="username" name="username" placeholder="username" value={this.state.username} onChange={this.handleChange} required/>
+          <input type="password" name="password" placeholder="Password" value={this.state.password} onChange={this.handleChange} required />
+          <button type="submit">Login</button>
+        </form>
+      </div>
     );
   }
 }
