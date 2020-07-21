9610732f13fc65b037242bb1bde7ee19333249dc
Nightrose2016 <lrbarrer@gmail.com>
2020-07-21 14:48:14 -0400

    Fixed copy paste

--- src/reducers/quizzesReducer.js
+++ src/reducers/quizzesReducer.js
@@ -1,8 +1,10 @@
 export default(state=[], action) => {
 
     switch(action.type){
-        case "SET_PETS":
+        case "SET_QUIZZES":
             return [...action.payload]
+        case "ADD_QUIZZES":
+            return [...state, action.payload]
         default:
             return state
     }
