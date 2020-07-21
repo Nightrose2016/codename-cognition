b0962b10b71402b31479a79c3bd073e95444c8bb
Nightrose2016 <lrbarrer@gmail.com>
2020-07-21 14:50:29 -0400

    Something change fuck if I know what ¯\_(ツ)_/¯

--- src/containers/QuizList.js
+++ src/containers/QuizList.js
@@ -3,25 +3,25 @@ import Quiz from '../components/Quiz'
 import { connect } from 'react-redux'
 
 class Quizzes extends Component {
-    render() {
-      const quizzes = this.props.quizzes.map(( quiz, i) => <Quiz key={i} quiz={ quiz } />)
-      return (
-        <div>
-          <h3>Quizzes</h3>
-          <ul className="collection">
-            { quizzes }
-          </ul>
-        </div>
-      )
-    }
+  render() {
+    const quizzes = this.props.quizzes.map(( quiz, i) => <Quiz key={i} quiz={ quiz } />)
+    return (
+      <div>
+        <h3>Quizzes</h3>
+        <ul className="collection">
+          { quizzes }
+        </ul>
+      </div>
+    )
   }
+}
   
-  const mapStateToProps = state => {
-    return {
-      quizzes: state.quizzes
-    }
+const mapStateToProps = state => {
+  return {
+    quizzes: state.quizzes
   }
+}
   
   
   
-  export default connect(mapStateToProps)(Quizzes)
+export default connect(mapStateToProps)(Quizzes)
