655ba661c5509834f3e011583cb8b4e992259898
Nightrose2016 <lrbarrer@gmail.com>
2020-07-20 21:05:12 -0400

    refactor quizactions

--- src/actions/quizActions.js
+++ src/actions/quizActions.js
@@ -1,10 +1,10 @@
 export const fetchQuizzes = () => {
     return (dispatch) => {
-      dispatch({ type: 'LOADING_Quizzes'})
-      fetch('http://localhost:3001/quizzes').then(response => {
-        return response.json()
-      }).then(responseJSON => {
-        dispatch({ type: 'ADD_Quizzes', quizzes: responseJSON })
+      // dispatch({ type: 'LOADING_Quizzes'})
+      return fetch('http://localhost:3001/quizzes')
+      .then(response => response.json())
+      .then(quizzes => {
+        dispatch({ type: 'SET_QUIZZES', payload: 'quizzes' })
       })
     }
   }
