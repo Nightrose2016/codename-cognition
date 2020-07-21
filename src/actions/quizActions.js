d7de1d81e7d7e9e4618fb32e83272270424eadf6
Nightrose2016 <lrbarrer@gmail.com>
2020-07-21 18:54:55 -0400

    removed unneeded code

--- src/actions/quizActions.js
+++ src/actions/quizActions.js
@@ -1,10 +1,9 @@
 export const fetchQuizzes = () => {
   return (dispatch) => {
-    // dispatch({ type: 'LOADING_Quizzes'})
     return fetch('http://localhost:3001/quizzes')
     .then(response => response.json())
     .then(quizzes => {
-      dispatch({ type: 'SET_QUIZZES', payload: 'quizzes' })
+      dispatch({ type: 'SET_QUIZZES', payload: quizzes })
     })
   }
 }
