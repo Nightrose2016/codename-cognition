d669fb964f56578b0ce5fc1a2ba37bba77ad5d9b
Nightrose2016 <lrbarrer@gmail.com>
2020-07-21 18:54:38 -0400

    Created and built exam actions

---
+++ src/actions/examsActions.js
@@ -0,0 +1,9 @@
+export const fetchExams = () => {
+    return (dispatch) => {
+      return fetch('http://localhost:3001/exams')
+      .then(response => response.json())
+      .then(exams => {
+        dispatch({ type: 'SET_EXAMS', payload: exams })
+      })
+    }
+  }
