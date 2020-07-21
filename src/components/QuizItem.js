6485560d9ed8715cc0bc98bb5bba0c31ed27261d
Nightrose2016 <lrbarrer@gmail.com>
2020-07-21 18:01:16 -0400

    renamed Quiz

--- src/components/QuizItem.js
+++ src/components/QuizItem.js
@@ -1,14 +1,15 @@
 import React from 'react'
+import { connect } from 'react-redux'
 // import { Link } from 'react-router-dom';
 
-const Quiz = ({ quiz }) => {
-    return (
+const QuizItem = ({ quiz }) => {
+  return (
     <li className="collection-item">
       Quiz Date: {quiz.quizdate}<br />
       Answer: { quiz.answer } <br />
       {/* <Link to={`/quizzes/${quiz.id}`}>View</Link> */}
     </li>
-    );
+  );
 }
 
-export default Quiz
+export default connect(QuizItem)
