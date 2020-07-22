01030af305ee284eb903c2c18297119a2ffd480f
Nightrose2016 <lrbarrer@gmail.com>
2020-07-22 00:13:31 -0400

    built retrogame list

---
+++ src/containers/RetrogameList.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+import RetrogameItem from '../components/items/RetrogameItem'
+import { connect } from 'react-redux'
+import { fetchRetrogames } from '../actions/retrogamesActions'
+
+class RetrogameList extends Component {
+
+    componentDidMount(){
+        this.props.fetchRetrogames()
+    }
+    
+    render() {
+        const retrogames = this.props.retrogames.map((retrogame, b) => <RetrogameItem key={b} retrogame={ retrogame }/>)
+        
+        return (
+            <div>
+                <h3 className="earwig">Retrogames</h3>
+                <ul className="retrogame-collection">
+                    { retrogames }
+                </ul>
+            </div>
+        )
+    }
+}
+  
+const mapStateToProps = state => {
+    return {
+        retrogames: state.retrogames
+    }
+}
+  
+  
+  
+export default connect(mapStateToProps, {fetchRetrogames})(RetrogameList)
