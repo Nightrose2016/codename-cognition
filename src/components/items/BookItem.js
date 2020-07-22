c12cc1976ee5cd79ebf2fc853c4753250ef30751
Nightrose2016 <lrbarrer@gmail.com>
2020-07-21 23:04:22 -0400

    created and built Book item

---
+++ src/components/items/BookItem.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { connect } from 'react-redux'
+
+const BookItem = ({ book }) => {
+    return (
+        <dl className="book-collection">
+            <dt>Book title: { book.name}</dt>
+            <dd>Boosted stat { book.boostedstat }</dd>
+            <dd>Day available: { book.availabledate }</dd>
+            <dd>chapters { book.chapters } </dd>
+            <dd>where to buy/borrow { book.location } </dd>
+        </dl>
+    );
+}
+
+export default connect(null)(BookItem)
