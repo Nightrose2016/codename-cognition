827b2ad2908c516f3b7315b404e369cffc39bedc
Nightrose2016 <lrbarrer@gmail.com>
2020-07-22 00:12:07 -0400

    added imports to index reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,17 @@
 import { combineReducers } from 'redux'
 import quizzes from './quizzesReducer'
 import exams from './examReducer'
+import retrogames from './retrogameReducer'
+import books from './bookReducer'
+import dvds from "./dvdReducer";
+import movies from "./movieReducer";
 
 export default combineReducers({
     quizzes,
-    exams
+    exams,
+    movies,
+    dvds,
+    books,
+    retrogames
 
 })
