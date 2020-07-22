5f3bff5013fe648f8e6c221ef58e6d0c63402fdb
Nightrose2016 <lrbarrer@gmail.com>
2020-07-21 23:05:04 -0400

    created and built movie item

---
+++ src/components/items/MovieItem.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { connect } from 'react-redux'
+
+const MovieItem = ({ book }) => {
+    return (
+        <dl className="movie-collection">
+            <dt>Movie title: { movie.name}</dt>
+            <dd>Boosted stat { movie.effect }</dd>
+            <dd>Screening: { movie.screening }</dd>
+            <dd>Location { movie.location } </dd>
+            <dd>Special event { movie.specialevent } </dd>
+        </dl>
+    );
+}
+
+export default connect(null)(MovieItem)
