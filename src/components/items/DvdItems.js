9a6402bdcd00197ad586561e7a773f2eb08420dc
Nightrose2016 <lrbarrer@gmail.com>
2020-07-21 23:04:36 -0400

    created and built dvd item

---
+++ src/components/items/DvdItems.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { connect } from 'react-redux'
+
+const DvdItem = ({ dvd }) => {
+    return (
+        <dl className="dvd-collection">
+            <dt>Dvd title: { dvd.name}</dt>
+            <dd>Boosted stat { dvd.boostedstat }</dd>
+            <dd>Day available: { dvd.availabledate }</dd>
+            <dd>Number of watches { dvd.episodes } </dd>
+        </dl>
+    );
+}
+
+export default connect(null)(DvdItem)
