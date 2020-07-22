726c27d04c9cb5cfd4a4da776ca20e60e4a49222
Nightrose2016 <lrbarrer@gmail.com>
2020-07-21 20:26:39 -0400

    Added a logo to nav bar and added quizzes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 import { NavLink } from 'react-router-dom';
+import gamelogo from '../styles/pictures/game-logo.png'
 
 export class NavBar extends Component {
   render() {
     return (
       <nav className="tbd">
         <div className="container">
-          <NavLink to="/" className="brand-logo">Persona 5 Royal</NavLink>
+          <NavLink to="/" className="game-logo"><img src={gamelogo}></img></NavLink>
+          <NavLink to="/quizzes" className="navbar">Quizzes</NavLink>
           <ul className="right">
            
           </ul>
