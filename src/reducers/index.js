86721bfa4deece048532b6726150e0d6a5a30606
Nightrose2016 <lrbarrer@gmail.com>
2020-07-21 21:23:50 -0400

    added exams to index reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux'
 import quizzes from './quizzesReducer'
+import exams from './examReducer'
 
 export default combineReducers({
-    quizzes
+    quizzes,
+    exams
 
 })
