67166bb56348b596e8b494be0de87420475ed436
Nightrose2016 <lrbarrer@gmail.com>
2020-07-22 00:27:12 -0400

    added links to nav bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -10,6 +10,10 @@ export class NavBar extends Component {
           <NavLink to="/" className="game-logo"><img src={gamelogo}></img></NavLink>
           <NavLink to="/quizzes" className="navbar">Quizzes</NavLink>
           <NavLink to="/exams" className="navbar">Exams</NavLink>
+          <NavLink to="/retrogames" className="navbar">Retro Games</NavLink>
+          <NavLink to="/dvds" className="navbar">DVDs</NavLink>
+          <NavLink to="/books" className="navbar">Books</NavLink>
+          <NavLink to="/movies" className="navbar">Movies</NavLink>
           <ul className="right">
            
           </ul>
