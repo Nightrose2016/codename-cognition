fb3ebaea7595a27d8f0d4c9ffa8b357e80444540
Nightrose2016 <lrbarrer@gmail.com>
2020-07-21 21:24:32 -0400

    Spacing fix to exams actions

--- src/actions/examsActions.js
+++ src/actions/examsActions.js
@@ -1,9 +1,9 @@
 export const fetchExams = () => {
     return (dispatch) => {
-      return fetch('http://localhost:3001/exams')
-      .then(response => response.json())
-      .then(exams => {
-        dispatch({ type: 'SET_EXAMS', payload: exams })
-      })
+        return fetch('http://localhost:3001/exams')
+        .then(response => response.json())
+        .then(exams => {
+            dispatch({ type: 'SET_EXAMS', payload: exams })
+        })
     }
   }
